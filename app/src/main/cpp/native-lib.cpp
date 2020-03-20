8e56e33f25c2b29902f44c98ba21c6ef9ccbecd9
Nelson <lilei@vsoontech.com>
2020-03-20 18:01:16 +0800

    完善第三方库的引用

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,29 @@
+#include <cstring>
 #include <jni.h>
+#include <cinttypes>
+#include <android/log.h>
+#include <gmath.h>
+#include <gperf.h>
 #include <string>
+
+#define LOGI(...) \
+  ((void)__android_log_print(ANDROID_LOG_INFO, "hello-libs::", __VA_ARGS__))
+
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_nelson_jniuselibs_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+
+    auto ticks = GetTicks();
+
+    for (auto exp = 0; exp < 32; ++exp) {
+        volatile unsigned val = gpower(exp);
+        (void) val;  // to silence compiler warning
+    }
+    ticks = GetTicks() - ticks;
+
+    LOGI("calculation time: %" PRIu64, ticks);
+
+    return env->NewStringUTF("Hello from JNI LIBS!");
 }
